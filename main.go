76c586612c07ff7a7b13e32d2df0b8fefbe84f57
Xavi Aznar <xavier.aznar@seat.es>
2024-06-22 14:08:59 +0000

    test: get ranges/rangebyid from IPAM Autopilot

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/xaviatwork/ipam-client/ipamclient"
+)
+
+func main() {
+	ipam := ipamclient.LzIpam{BaseUrl: os.Getenv("IPAM_URL")}
+	if ipam.BaseUrl == "" {
+		fmt.Println("No IPAM url provided. Please set IPAM_URL env variable")
+		os.Exit(1)
+	}
+	lzranges, err := ipam.Ranges()
+	if err != nil {
+		fmt.Printf("IPAM returned %s\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(lzranges)
+
+	lzrange, err := ipam.RangeById(2)
+	if err != nil {
+		fmt.Printf("IPAM returned %s\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("IPAM Range: %+v\n", lzrange)
+}
