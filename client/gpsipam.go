2b1bac2448b87a23134f82f9524aa50c8ab1b596
Xavi Aznar <xavier.aznar@seat.es>
2024-06-29 17:48:40 +0000

    refactor: -free is a flag for -id <int> (also homogenize variable names)

--- client/gpsipam.go
+++ client/gpsipam.go
@@ -107,7 +107,7 @@ func (gpsipam GpsIpam) Status() error {
 
 func GetRangeById(ipam ipamautopilot.Ipam, opts Opts) {
 	iprange, err := ipam.RangeById(opts.Id)
-	iprng := Pokemonize(iprange)
+	iprng := Anonymize(iprange)
 	if err != nil {
 		fmt.Printf("IPAM response: %s\n", err.Error())
 		os.Exit(1)
@@ -126,7 +126,7 @@ func GetRangesWithParent(ipam ipamautopilot.Ipam, opts Opts) {
 		os.Exit(1)
 	}
 	for _, r := range *ipRanges {
-		pkr := Pokemonize(&r)
+		pkr := Anonymize(&r)
 		if pkr.Parent_id == opts.Parent {
 			if opts.Pretty {
 				fmt.Printf("%s\n", pkr.PrettyString())
@@ -144,7 +144,7 @@ func SearchStringInRanges(ipam ipamautopilot.Ipam, opts Opts) {
 		os.Exit(1)
 	}
 	for _, r := range *ipRanges {
-		pkr := Pokemonize(&r)
+		pkr := Anonymize(&r)
 		if searchString(opts.SearchString, pkr.Name, pkr.Cidr) {
 			if opts.Pretty {
 				fmt.Printf("%s\n", pkr.PrettyString())
@@ -169,58 +169,52 @@ func GetDomainById(ipam ipamautopilot.Ipam, opts Opts) {
 }
 
 func GetNonAllocatedIPs(ipam ipamautopilot.Ipam, opts Opts) {
-	iprange, err := ipam.RangeById(opts.Free)
+	mr, err := ipam.RangeById(opts.Id)
 	if err != nil {
 		fmt.Printf("IPAM response: %s\n", err.Error())
 		os.Exit(1)
 	}
-	pokeRange := Pokemonize(iprange)
+	mainRange := Anonymize(mr)
 
-	if pokeRange.Parent_id != -1 {
-		fmt.Printf("Range %d (name: %s) is not a main range\n", opts.Free, pokeRange.Name)
+	if mainRange.Parent_id != -1 {
+		fmt.Printf("Range %d (name: %s) is not a main range\n", opts.Id, mainRange.Name)
 		os.Exit(1)
 	}
 
 	// lleig
-	s := strings.SplitAfter(pokeRange.Cidr, "/")
-	strblock := s[len(s)-1]
-	size, _ := strconv.Atoi(strblock)
-	ipsOnRange := math.Pow(2, float64(32)-float64(size))
+	s := strings.SplitAfter(mainRange.Cidr, "/")
+	sizeMainRange := s[len(s)-1]
+	size, _ := strconv.Atoi(sizeMainRange)
+	ipsMainRange := math.Pow(2, float64(32)-float64(size))
+	availableIPs := ipsMainRange
 
-	subranges, err := ipam.Ranges()
+	srs, err := ipam.Ranges()
 	if err != nil {
 		log.Printf("error: %s", err.Error())
 	}
-	available := ipsOnRange
+
 	var allocated float64
-
-	pokerange := Pokemonize(&pokeRange)
-
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"CIDR", "Allocated IPs", "Available IPs", "CIDR Name"})
-
-	t.AppendRow(table.Row{pokerange.Cidr, 0, available, pokerange.Name})
+	t.AppendRow(table.Row{mainRange.Cidr, 0, availableIPs, mainRange.Name})
 	t.AppendSeparator()
 
-	// fmt.Printf("total IPs: %.0f, CIDR: %s (%q))\n", ipsOnRange, iprange.Cidr, iprange.Name)
-	for _, sr := range *subranges {
-		if sr.Parent_id == opts.Free {
-			pokeSubnet := Pokemonize(&sr)
-			s := strings.SplitAfter(pokeSubnet.Cidr, "/")
+	for _, sr := range *srs {
+		if sr.Parent_id == opts.Id {
+			subnetRange := Anonymize(&sr)
+			s := strings.SplitAfter(subnetRange.Cidr, "/")
 			strblock := s[len(s)-1]
 			size, _ := strconv.Atoi(strblock)
-			ipsOnSubnet := math.Pow(2, float64(32)-float64(size))
-			// fmt.Printf("ips on subnet %.0f (CIDR: %s)\n", ipsOnSubnet, sr.Cidr)
-			available = available - ipsOnSubnet
-			allocated = allocated + ipsOnSubnet
-			// fmt.Printf("Available %4.0f (after allocating %3.0f IPs, %s)\n", available, ipsOnSubnet, sr.Cidr)
+			ipsSubnet := math.Pow(2, float64(32)-float64(size))
+			availableIPs = availableIPs - ipsSubnet
+			allocated = allocated + ipsSubnet
 
-			t.AppendRow(table.Row{pokeSubnet.Cidr, ipsOnSubnet, available, pokeSubnet.Name})
+			t.AppendRow(table.Row{subnetRange.Cidr, ipsSubnet, availableIPs, subnetRange.Name})
 		}
 	}
 	t.AppendSeparator()
-	t.AppendRow(table.Row{"Total", allocated, available, ""})
+	t.AppendRow(table.Row{"Total", allocated, availableIPs, ""})
 	t.SetStyle(table.StyleLight)
 	t.Render()
 }
@@ -254,7 +248,7 @@ func searchString(ss ...string) bool {
 	return found
 }
 
-func Pokemonize(r *ipamautopilot.Range) ipamautopilot.Range {
+func Anonymize(r *ipamautopilot.Range) ipamautopilot.Range {
 	if os.Getenv("POKEMONIZE") == "yes" {
 		pokemons := []string{"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch’d", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr._Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew", "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom", "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar", "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery", "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-Oh", "Celebi", "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon", "Linoone", "Wurmple", "Silcoon", "Beautifly", "Cascoon", "Dustox", "Lotad", "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit", "Masquerain", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking", "Nincada", "Ninjask", "Shedinja", "Whismur", "Loudred", "Exploud", "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty", "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham", "Electrike", "Manectric", "Plusle", "Minun", "Volbeat", "Illumise", "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer", "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda", "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria", "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash", "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily", "Anorith", "Armaldo", "Feebas", "Milotic", "Castform", "Kecleon", "Shuppet", "Banette", "Duskull", "Dusclops", "Tropius", "Chimecho", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein", "Clamperl", "Huntail", "Gorebyss", "Relicanth", "Luvdisc", "Bagon", "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys", "Turtwig", "Grotle", "Torterra", "Chimchar", "Monferno", "Infernape", "Piplup", "Prinplup", "Empoleon", "Starly", "Staravia", "Staraptor", "Bidoof", "Bibarel", "Kricketot", "Kricketune", "Shinx", "Luxio", "Luxray", "Budew", "Roserade", "Cranidos", "Rampardos", "Shieldon", "Bastiodon", "Burmy", "Wormadam", "Mothim", "Combee", "Vespiquen", "Pachirisu", "Buizel", "Floatzel", "Cherubi", "Cherrim", "Shellos", "Gastrodon", "Ambipom", "Drifloon", "Drifblim", "Buneary", "Lopunny", "Mismagius", "Honchkrow", "Glameow", "Purugly", "Chingling", "Stunky", "Skuntank", "Bronzor", "Bronzong", "Bonsly", "Mime_Jr.", "Happiny", "Chatot", "Spiritomb", "Gible", "Gabite", "Garchomp", "Munchlax", "Riolu", "Lucario", "Hippopotas", "Hippowdon", "Skorupi", "Drapion", "Croagunk", "Toxicroak", "Carnivine", "Finneon", "Lumineon", "Mantyke", "Snover", "Abomasnow", "Weavile", "Magnezone", "Lickilicky", "Rhyperior", "Tangrowth", "Electivire", "Magmortar", "Togekiss", "Yanmega", "Leafeon", "Glaceon", "Gliscor", "Mamoswine", "Porygon-Z", "Gallade", "Probopass", "Dusknoir", "Froslass", "Rotom", "Uxie", "Mesprit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Phione", "Manaphy", "Darkrai", "Shaymin", "Arceus", "Victini", "Snivy", "Servine", "Serperior", "Tepig", "Pignite", "Emboar", "Oshawott", "Dewott", "Samurott", "Patrat", "Watchog", "Lillipup", "Herdier", "Stoutland", "Purrloin", "Liepard", "Pansage", "Simisage", "Pansear", "Simisear", "Panpour", "Simipour", "Munna", "Musharna", "Pidove", "Tranquill", "Unfezant", "Blitzle", "Zebstrika", "Roggenrola", "Boldore", "Gigalith", "Woobat", "Swoobat", "Drilbur", "Excadrill", "Audino", "Timburr", "Gurdurr", "Conkeldurr", "Tympole", "Palpitoad", "Seismitoad", "Throh", "Sawk", "Sewaddle", "Swadloon", "Leavanny", "Venipede", "Whirlipede", "Scolipede", "Cottonee", "Whimsicott", "Petilil", "Lilligant", "Basculin", "Sandile", "Krokorok", "Krookodile", "Darumaka", "Darmanitan", "Maractus", "Dwebble", "Crustle", "Scraggy", "Scrafty", "Sigilyph", "Yamask", "Cofagrigus", "Tirtouga", "Carracosta", "Archen", "Archeops", "Trubbish", "Garbodor", "Zorua", "Zoroark", "Minccino", "Cinccino", "Gothita", "Gothorita", "Gothitelle", "Solosis", "Duosion", "Reuniclus", "Ducklett", "Swanna", "Vanillite", "Vanillish", "Vanilluxe", "Deerling", "Sawsbuck", "Emolga", "Karrablast", "Escavalier", "Foongus", "Amoonguss", "Frillish", "Jellicent", "Alomomola", "Joltik", "Galvantula", "Ferroseed", "Ferrothorn", "Klink", "Klang", "Klinklang", "Tynamo", "Eelektrik", "Eelektross", "Elgyem", "Beheeyem", "Litwick", "Lampent", "Chandelure", "Axew", "Fraxure", "Haxorus", "Cubchoo", "Beartic", "Cryogonal", "Shelmet", "Accelgor", "Stunfisk", "Mienfoo", "Mienshao", "Druddigon", "Golett", "Golurk", "Pawniard", "Bisharp", "Bouffalant", "Rufflet", "Braviary", "Vullaby", "Mandibuzz", "Heatmor", "Durant", "Deino", "Zweilous", "Hydreigon", "Larvesta", "Volcarona", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Keldeo", "Meloetta", "Genesect", "Chespin", "Quilladin", "Chesnaught", "Fennekin", "Braixen", "Delphox", "Froakie", "Frogadier", "Greninja", "Bunnelby", "Diggersby", "Fletchling", "Fletchinder", "Talonflame", "Scatterbug", "Spewpa", "Vivillon", "Litleo", "Pyroar", "Flabébé", "Floette", "Florges", "Skiddo", "Gogoat", "Pancham", "Pangoro", "Furfrou", "Espurr", "Meowstic", "Honedge", "Doublade", "Aegislash", "Spritzee", "Aromatisse", "Swirlix", "Slurpuff", "Inkay", "Malamar", "Binacle", "Barbaracle", "Skrelp", "Dragalge", "Clauncher", "Clawitzer", "Helioptile", "Heliolisk", "Tyrunt", "Tyrantrum", "Amaura", "Aurorus", "Sylveon", "Hawlucha", "Dedenne", "Carbink", "Goomy", "Sliggoo", "Goodra", "Klefki", "Phantump", "Trevenant", "Pumpkaboo", "Gourgeist", "Bergmite", "Avalugg", "Noibat", "Noivern", "Xerneas", "Yveltal", "Zygarde", "Diancie", "Hoopa", "Volcanion", "Rowlet", "Dartrix", "Decidueye", "Litten", "Torracat", "Incineroar", "Popplio", "Brionne", "Primarina", "Pikipek", "Trumbeak", "Toucannon", "Yungoos", "Gumshoos", "Grubbin", "Charjabug", "Vikavolt", "Crabrawler", "Crabominable", "Oricorio", "Cutiefly", "Ribombee", "Rockruff", "Lycanroc", "Wishiwashi", "Mareanie", "Toxapex", "Mudbray", "Mudsdale", "Dewpider", "Araquanid", "Fomantis", "Lurantis", "Morelull", "Shiinotic", "Salandit", "Salazzle", "Stufful", "Bewear", "Bounsweet", "Steenee", "Tsareena", "Comfey", "Oranguru", "Passimian", "Wimpod", "Golisopod", "Sandygast", "Palossand", "Pyukumuku", "Type:_Null", "Silvally", "Minior", "Komala", "Turtonator", "Togedemaru", "Mimikyu", "Bruxish", "Drampa", "Dhelmise", "Jangmo-o", "Hakamo-o", "Kommo-o", "Tapu_Koko", "Tapu_Lele", "Tapu_Bulu", "Tapu_Fini", "Cosmog", "Cosmoem", "Solgaleo", "Lunala", "Nihilego", "Buzzwole", "Pheromosa", "Xurkitree", "Celesteela", "Kartana", "Guzzlord", "Necrozma", "Magearna", "Marshadow", "Poipole", "Naganadel", "Stakataka", "Blacephalon", "Zeraora", "Meltan", "Melmetal"}
 		s := strings.SplitAfter(r.Cidr, "/")
--- main.go
+++ main.go
@@ -33,11 +33,13 @@ func main() {
 		case opts.Id != 0:
 			// if the range is not found, IPAM Autopilot returns a 503 Service Unavailable error
 			// https://github.com/GoogleCloudPlatform/professional-services/blob/main/tools/ipam-autopilot/container/api.go#L81
+			if opts.Free {
+				client.GetNonAllocatedIPs(ipam, *opts)
+				os.Exit(0)
+			}
 			client.GetRangeById(ipam, *opts)
 		case opts.Parent != 0:
 			client.GetRangesWithParent(ipam, *opts)
-		case opts.Free != 0:
-			client.GetNonAllocatedIPs(ipam, *opts)
 		case opts.SearchString != "":
 			client.SearchStringInRanges(ipam, *opts)
 		default: // all ranges
