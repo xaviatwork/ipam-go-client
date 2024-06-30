1abd2325f879cf9c4666ddc2d3206542c0970f22
Xavi Aznar <xavier.aznar@seat.es>
2024-06-30 16:39:24 +0000

    chore: add delta (diff tool)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get install make --yes
 grep --quiet --fixed-strings --line-regexp 'source .devcontainer/git-completion.bash' ~/.bashrc || echo 'source .devcontainer/git-completion.bash' >> ~/.bashrc
+curl -q -JLO https://github.com/dandavison/delta/releases/download/0.17.0/git-delta_0.17.0_amd64.deb && sudo dpkg -i git-delta_0.17.0_amd64.deb
